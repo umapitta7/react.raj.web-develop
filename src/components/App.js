35c089d881eb5331a0694714aea442da9a0c98e5
umapitta7 <81534212+umapitta7@users.noreply.github.com>
2021-06-14 22:39:00 -0500

    Add files via upload

---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default class App extends React.PureComponent {
+  render() {
+    return (
+      <div>
+        <div id="root1">test</div>
+      </div>
+    );
+  }
+}
